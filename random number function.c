62ca272572f5a8ad13c5638d950af9c7ba67b67f
Ernesto Topete <ejttje@gmail.com>
2012-01-29 23:14:21 -0800

    randomize your mind...

--- random number function.c
+++ random number function.c
@@ -7,6 +7,8 @@ int main(void)
     
     while (i < 15){
           /*random function code =(rand)*/
+          /* [printf("%d\n", 1+rand()%6);]... add or take numbers away as needed to get desired output from random generator since "0" is considered as a possible outcome.*/
+          
     printf("%d\n", 1+rand()%6);
     i++;
 }

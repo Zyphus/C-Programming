c9e77549a3166bcf05f69b991ea9f3ffc802e6cd
Ernesto Topete <ejttje@gmail.com>
2012-01-29 23:50:26 -0800

    intro-arrays

--- intro to arrays.c
+++ intro to arrays.c
@@ -4,12 +4,14 @@
 int main(void)
 {
     /*name array*/
+    /*this is a shortcut instead of figure 1.0*/
     int pikus[5]={21,13,435,56,7};
     
     printf("%d\n", pikus[1]);
     
     
     /*assign values
+    FIG 1.0
     pikus[0]=16;
     pikus[1]=21;
     /*there is an easier way to assign values

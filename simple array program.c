9b728c8124aa46ed944e37a6b6c9fb84dfd34f86
Ernesto Topete <ejttje@gmail.com>
2012-01-29 23:59:38 -0800

    array program

--- simple array program.c
+++ simple array program.c
@@ -3,10 +3,14 @@
 
 int main(void)
 {
+    /*set array values 0-1-2-3-4...*/
    int pikus[5]={16,21,30,35,40};
    int i;
    int total=0;
-   
+   /*i will equal 0 then move to the next array value
+   which equals 1 then 2 and so on....
+   until it reaches 5 or 4 in assembly since 0 is considered
+   as a starting point.*/
    for(i=0;i<5;i++){
    total +=pikus[i];
    }

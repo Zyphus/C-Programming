d74bf8db6b8e6f9fcfdca83c82bac1d00d8f8266
Ernesto Topete <ejttje@gmail.com>
2012-02-01 19:59:02 -0800

    new changes

--- switch statement.c
+++ switch statement.c
@@ -11,21 +11,10 @@
 
 int main(void)
          {
-int number;
-scanf("%d", &number);
+do{
+
          
-switch(number){
-case 2:
-printf("You entered 2");
-break;    
-case 4:
-printf("You entered 4");
-break;
-case 6:
-printf("You entered 6");
-break;    
-default:
-printf("I dont know what you entered");
+         }
  
          }
          
